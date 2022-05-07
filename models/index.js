bb522fe769568fc6c4ed30844dc1ab68306cd0d2
ravnishgupta <ravnish@gmail.com>
2022-05-07 08:35:01 -0700

    adding constraints between user and projects

--- models/index.js
+++ models/index.js
@@ -4,4 +4,15 @@ const Skill = require('./Skill');
 const ProjectSkill = require('./ProjectSkill')
 const UserSkill = require('./UserSkill')
 
+User.hasMany(UserSkill, {
+    foreignKey: 'userID'
+})
+
+Project.hasMany(ProjectSkill, {
+    foreignKey: 'projectID'
+})
+
+
+
+
 module.exports = { User, Project, Skill, ProjectSkill, UserSkill };
