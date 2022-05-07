d08f16c6919e701f74935e3eb9eecbd79398c600
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-05-07 14:49:34 -0700

    Initial contraints Projects and Skills

--- models/index.js
+++ models/index.js
@@ -4,15 +4,14 @@ const Skill = require('./Skill');
 const ProjectSkill = require('./ProjectSkill')
 const UserSkill = require('./UserSkill')
 
-User.hasMany(UserSkill, {
-    foreignKey: 'userID'
-})
-
-Project.hasMany(ProjectSkill, {
-    foreignKey: 'projectID'
-})
+User.belongsToMany(Skill, {through: UserSkill, foreignKey: 'userId'});
+Skill.belongsToMany(User, {through: UserSkill, foreignKey: 'skillId'});
 
 
+Project.belongsToMany(Skill, {through: ProjectSkill, foreignKey: 'projectId'});
+Skill.belongsToMany(Project, {through: ProjectSkill, foreignKey: 'skillId'});
+
+// Project.hasMany(User, {foreignKey: 'userId'})
 
 
 module.exports = { User, Project, Skill, ProjectSkill, UserSkill };
