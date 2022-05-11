3e08e043e55ddf2b181baa085d77454a48e42df3
nataliesleem <natalie.sleem@yahoo.com>
2022-05-10 20:34:02 -0700

    added apply model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
     res.render('signup');	
 });
-
+router.get('/login', async (req, res) => {
+    res.render('login');	
+});
 
 router.get('/projects', async (req, res) => {
 
