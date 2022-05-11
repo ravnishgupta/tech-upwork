7af1589b7cacfbf0a610caac01720aadacace3d1
nataliesleem <natalie.sleem@yahoo.com>
2022-05-10 20:42:34 -0700

    added userproject association

--- models/index.js
+++ models/index.js
@@ -3,15 +3,17 @@ const Project = require("./Project");
 const Skill = require('./Skill');
 const ProjectSkill = require('./ProjectSkill')
 const UserSkill = require('./UserSkill')
+const Apply = require("./Apply")
 
 User.belongsToMany(Skill, {through: UserSkill, foreignKey: 'userId'});
 Skill.belongsToMany(User, {through: UserSkill, foreignKey: 'skillId'});
+
+User.belongsToMany(Project, {through: Apply, foreignKey:'userId'});
+Project.belongsToMany(User, {through: Apply, foreignKey: 'projectId'});
 
 
 Project.belongsToMany(Skill, {through: ProjectSkill, foreignKey: 'projectId'});
 Skill.belongsToMany(Project, {through: ProjectSkill, foreignKey: 'skillId'});
 
-// Project.hasMany(User, {foreignKey: 'userId'});
-// User.belongsTo(Project, {foreignKey:'projectId'});
 
-module.exports = { User, Project, Skill, ProjectSkill, UserSkill };
+module.exports = { User, Project, Skill, ProjectSkill, UserSkill, Apply};
