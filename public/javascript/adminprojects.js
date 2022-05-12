ba9da9b02ad98f38dfa6e4ee9463d6c1af3b8764
meghark <98137756+meghark@users.noreply.github.com>
2022-05-11 20:36:07 -0700

    Added new project page for admin

--- public/javascript/adminprojects.js
+++ public/javascript/adminprojects.js
@@ -5,23 +5,7 @@ const viewButtonHandler = async (event) =>{
 
     const projectId = event.target.getAttribute('data-id');
 
-    if(projectId)
-    {
-        const response = await fetch(`/file/${userId}`);
-        console.log(response);
-        const r = await response.blob();
-        console.log(r);
-
-        const url = window.URL.createObjectURL(r);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'resume.pdf');
-        document.body.appendChild(link);
-        link.click();
-
-        link.parentNode.removeChild(link);
-
-    };
+   
 
  };
 
