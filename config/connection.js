e0e84d9f4b7df6ed55ea8e0a150ca8d1b4371ad7
meghark <98137756+meghark@users.noreply.github.com>
2022-05-15 21:02:49 -0700

    For deployment on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+// Use jawsdb if deploying to heroku
+
+if(process.env.JAWSDB_URL)
+{
+    sequelize = new sequelize(process.env.JAWSDB_URL);
+}
+else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      });
+}
+
+
 
 module.exports = sequelize;
